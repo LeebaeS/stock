2aa9807fc50a94f0c2554b998ae17321d0481fd7
LeebaeS <aranga0419@gmail.com>
2025-03-25 17:46:57 +0900

    Add files via upload

---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.views.generic import View, TemplateView
+
+# Create your views here.
+# views.py
+
+
+class SignUpView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'accounts/signup.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'accounts/signup.html', {'form': form})
+    
+class CustomLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    success_url = reverse_lazy('home')
+
+class CustomPasswordResetView(PasswordResetView):
+    template_name = 'accounts/password_reset_form.html'
+
+class CustomPasswordResetDoneView(PasswordResetDoneView):
+    template_name = 'accounts/password_reset_done.html'
+
+class CustomPasswordResetConfirmView(PasswordResetConfirmView):
+    template_name = 'accounts/password_reset_confirm.html'
+
+class CustomPasswordResetCompleteView(PasswordResetCompleteView):
+    template_name = 'accounts/password_reset_complete.html'
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
